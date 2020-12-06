5f26fb2be1ca5934d9bdec5666bca828a2d1c4e7
Paul <paul.sh055@gmail.com>
2020-12-06 23:43:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя {0}, Ваш возраст {1}", name, age);
+            Console.Write("Введите дату рождения: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine($"Ваша дата рождения {birthdate}");
+            Console.ReadKey();
+        }
+    }
+}
